78a8956c3ab3f029f5b78c6adb9cc4c01e63fc03
Mohamed Shimash <shimash.ca@gmail.com>
2024-04-06 06:49:07 -0400

    Ready to Run
    The complete working system of the Hair Salon Application with successful passing of the testing.

---
+++ HairSalon.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Lab3B
+{
+    partial class HairSalon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.hairdresserCombo = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.serviceList = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.chargedItemsList = new System.Windows.Forms.ListBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.priceList = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.totaltxt = new System.Windows.Forms.TextBox();
+            this.serviceBtn = new System.Windows.Forms.Button();
+            this.totalBtn = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.hairdresserCombo);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 190);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select a Hairdresser:";
+            // 
+            // hairdresserCombo
+            // 
+            this.hairdresserCombo.DisplayMember = "Jane";
+            this.hairdresserCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.hairdresserCombo.FormattingEnabled = true;
+            this.hairdresserCombo.Items.AddRange(new object[] {
+            "Jane",
+            "Pat",
+            "Ron",
+            "Sue",
+            "Larie"});
+            this.hairdresserCombo.Location = new System.Drawing.Point(7, 26);
+            this.hairdresserCombo.Name = "hairdresserCombo";
+            this.hairdresserCombo.Size = new System.Drawing.Size(187, 27);
+            this.hairdresserCombo.TabIndex = 0;
+            this.hairdresserCombo.SelectedIndexChanged += new System.EventHandler(this.hairdresserCombo_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.serviceList);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(228, 24);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(240, 190);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Select a Service:";
+            // 
+            // serviceList
+            // 
+            this.serviceList.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serviceList.ItemHeight = 19;
+            this.serviceList.Items.AddRange(new object[] {
+            "Cut",
+            "Wash, blow-dry, and style",
+            "Colour",
+            "Highlights",
+            "Extension",
+            "Up-do"});
+            this.serviceList.Location = new System.Drawing.Point(7, 26);
+            this.serviceList.Name = "serviceList";
+            this.serviceList.Size = new System.Drawing.Size(227, 156);
+            this.serviceList.TabIndex = 0;
+            this.serviceList.SelectedIndexChanged += new System.EventHandler(this.serviceList_SelectedIndexChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.chargedItemsList);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(485, 24);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(240, 190);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Charged Items:";
+            // 
+            // chargedItemsList
+            // 
+            this.chargedItemsList.Enabled = false;
+            this.chargedItemsList.FormattingEnabled = true;
+            this.chargedItemsList.ItemHeight = 19;
+            this.chargedItemsList.Location = new System.Drawing.Point(6, 26);
+            this.chargedItemsList.Name = "chargedItemsList";
+            this.chargedItemsList.Size = new System.Drawing.Size(228, 156);
+            this.chargedItemsList.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.priceList);
+            this.groupBox4.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox4.Location = new System.Drawing.Point(741, 24);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(120, 190);
+            this.groupBox4.TabIndex = 0;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Price:";
+            // 
+            // priceList
+            // 
+            this.priceList.Enabled = false;
+            this.priceList.FormattingEnabled = true;
+            this.priceList.ItemHeight = 19;
+            this.priceList.Location = new System.Drawing.Point(7, 27);
+            this.priceList.Name = "priceList";
+            this.priceList.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.priceList.Size = new System.Drawing.Size(107, 156);
+            this.priceList.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ebrima", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(658, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Total Price:";
+            // 
+            // totaltxt
+            // 
+            this.totaltxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totaltxt.Location = new System.Drawing.Point(748, 229);
+            this.totaltxt.Multiline = true;
+            this.totaltxt.Name = "totaltxt";
+            this.totaltxt.ReadOnly = true;
+            this.totaltxt.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.totaltxt.Size = new System.Drawing.Size(107, 30);
+            this.totaltxt.TabIndex = 2;
+            // 
+            // serviceBtn
+            // 
+            this.serviceBtn.Font = new System.Drawing.Font("Ebrima", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serviceBtn.Location = new System.Drawing.Point(228, 316);
+            this.serviceBtn.Name = "serviceBtn";
+            this.serviceBtn.Size = new System.Drawing.Size(147, 33);
+            this.serviceBtn.TabIndex = 3;
+            this.serviceBtn.Text = "Add Service";
+            this.serviceBtn.UseVisualStyleBackColor = true;
+            this.serviceBtn.Click += new System.EventHandler(this.serviceBtn_Click);
+            // 
+            // totalBtn
+            // 
+            this.totalBtn.Font = new System.Drawing.Font("Ebrima", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalBtn.Location = new System.Drawing.Point(386, 316);
+            this.totalBtn.Name = "totalBtn";
+            this.totalBtn.Size = new System.Drawing.Size(147, 33);
+            this.totalBtn.TabIndex = 3;
+            this.totalBtn.Text = "Calculate Total";
+            this.totalBtn.UseVisualStyleBackColor = true;
+            this.totalBtn.Click += new System.EventHandler(this.totalBtn_Click);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Font = new System.Drawing.Font("Ebrima", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetBtn.Location = new System.Drawing.Point(545, 316);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(147, 33);
+            this.resetBtn.TabIndex = 3;
+            this.resetBtn.Text = "Reset";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Font = new System.Drawing.Font("Ebrima", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.Location = new System.Drawing.Point(704, 316);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(147, 33);
+            this.exitBtn.TabIndex = 3;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // HairSalon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 361);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.totalBtn);
+            this.Controls.Add(this.serviceBtn);
+            this.Controls.Add(this.totaltxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "HairSalon";
+            this.Text = "Perfect Cut Hair Salon";
+            this.Load += new System.EventHandler(this.HairSalon_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ComboBox hairdresserCombo;
+        private System.Windows.Forms.ListBox serviceList;
+        private System.Windows.Forms.ListBox chargedItemsList;
+        private System.Windows.Forms.ListBox priceList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox totaltxt;
+        private System.Windows.Forms.Button serviceBtn;
+        private System.Windows.Forms.Button totalBtn;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.Button exitBtn;
+    }
+}
+
---
+++ HairSalon.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3B
+{
+    //Statement of Authorship: I, Mohamed Shimash Mohamed Sirajul Hasan, 000899687 certify that this material is my original work.No other person's work has been used without due acknowledgement.
+    //This is a Windows Forms application which calculates the total price of a Hair Salon Services, created on 31.10.2023 
+    public partial class HairSalon : Form
+    {
+        
+
+        public HairSalon()
+        {
+            InitializeComponent();
+        }
+
+        //variable to hold hairdresser price
+        double hairDresser;
+        //variable to hold service price
+        double service;
+        //variable to hold total price
+        double sum;
+        private void hairdresserCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        // event handler to calculate total price when 'Calculate Total' button is clicked
+        private void totalBtn_Click(object sender, EventArgs e)
+        {
+            sum = 0;
+
+            foreach (object obj in priceList.Items)
+            {
+                sum += Convert.ToDouble(obj.ToString().Substring(1));
+            }
+
+            totaltxt.Text = sum.ToString("$" + "0.00");
+        }
+
+        //event handler to defalut form load
+        private void HairSalon_Load(object sender, EventArgs e)
+        {
+            hairdresserCombo.SelectedItem = "Jane";
+            hairdresserCombo.Focus();
+            serviceBtn.Enabled = false;
+            totalBtn.Enabled = false;
+        }
+
+        // event handler to exit the program when 'Exit' button is clicked
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // event handler when a service is selected from the service list
+        private void serviceList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (serviceList.SelectedItem != null)
+            {
+                serviceBtn.Enabled = true;
+            }
+            else
+            {
+                serviceBtn.Enabled = false;
+            }
+        }
+
+        // event handler when 'Add service' button is clicked
+        private void serviceBtn_Click(object sender, EventArgs e)
+        {
+            // assigning prices based on selected hairdresser
+            if (hairdresserCombo.Text == "Jane")
+            {
+                hairDresser = 30;
+            }
+            else if (hairdresserCombo.Text == "Pat")
+            {
+                hairDresser = 45;
+            }
+            else if (hairdresserCombo.Text == "Ron")
+            {
+                hairDresser = 40;
+            }
+            else if (hairdresserCombo.Text == "Sue")
+            {
+                hairDresser = 50;
+            }
+            else if (hairdresserCombo.Text == "Laurie")
+            {
+                hairDresser = 55;
+            }
+
+            // adding selected service to the list
+            if (priceList.Items.Count == 0)
+            {
+                chargedItemsList.Items.Add(hairdresserCombo.SelectedItem);
+                priceList.Items.Add(hairDresser.ToString("$" + "0.00"));
+                hairdresserCombo.Enabled = false;
+                totalBtn.Enabled = true;
+            }
+
+
+
+            // assigning prices based on selected service
+            if (serviceList.Text == "Cut")
+            {
+                service = 30;
+                chargedItemsList.Items.Add(serviceList.Text);
+                priceList.Items.Add(service.ToString("$" + "0.00"));
+            }
+            if (serviceList.Text == "Wash, blow-dry, and style")
+            {
+                service = 20;
+                chargedItemsList.Items.Add(serviceList.Text);
+                priceList.Items.Add(service.ToString("$" + "0.00"));
+            }
+            if (serviceList.Text == "Colour")
+            {
+                service = 40;
+                chargedItemsList.Items.Add(serviceList.Text);
+                priceList.Items.Add(service.ToString("$" + "0.00"));
+            }
+            if (serviceList.Text == "Highlights")
+            {
+                service = 50;
+                chargedItemsList.Items.Add(serviceList.Text);
+                priceList.Items.Add(service.ToString("$" + "0.00"));
+            }
+            if (serviceList.Text == "Extension")
+            {
+                service = 200;
+                chargedItemsList.Items.Add(serviceList.Text);
+                priceList.Items.Add(service.ToString("$" + "0.00"));
+            }
+            if (serviceList.Text == "Up-do")
+            {
+                service = 60;
+                chargedItemsList.Items.Add(serviceList.Text);
+                priceList.Items.Add(service.ToString("$" + "0.00"));
+            }
+            serviceBtn.Focus();
+        }
+
+        // event handler to reset the program to default when 'reset' button is clicked
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            hairdresserCombo.SelectedItem = "Jane";
+            hairdresserCombo.Enabled = true;
+            hairdresserCombo.Focus();
+
+            serviceList.SelectedItem = "";
+            chargedItemsList.Items.Clear();
+            priceList.Items.Clear();
+            serviceBtn.Enabled = false;
+            totalBtn.Enabled = false;
+            totaltxt.Text = "";
+            
+        }
+    }
+}
+
+
+
+
+
+
